3f68696cb23b52d11908172496c4f668a66ce260
Gelado047 <juniorthesouza017@gmail.com>
2023-08-19 22:17:11 -0300

    Finish

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CGPA_Calculator
 Calculator
+
+# Objetivo
+Praticando conhecimentos gerais utilizando a tecnologia C++ em busca de conhecimentos em linguagens orientadas a objetos
--- src/index.cpp
+++ src/index.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -18,40 +19,68 @@ int main() {
     }
     if(percent > 0){
         if((percent >= 90) && (percent <= 100)){
-
             gpa = 4.00;
-            cout<<"Your GPA is: "<< gpa<< endl;
+
+            cout<<"Your GPA is: "<<setprecision(2)<<gpa<< endl;
             cout<<"Your Grade is \"A+\" "<<endl;
-            cout<<"Your Percentage is: "<< percent<<endl;
-        }
-         else if((percent >= 85) && (percent <= 89.99)){
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
 
+        }else if((percent >= 85) && (percent <= 89.99)){
             gpa = 4.00;
-            cout<<"Your GPA is: "<< gpa<< endl;
+
+            cout<<"Your GPA is: "<<setprecision(2)<< gpa<< endl;
             cout<<"Your Grade is \"A\" "<< endl;
-            cout<<"Your Percentage is: "<< percent<< endl;
-        }
-         else if((percent >= 80) && (percent <= 84.99)){
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
 
+        }else if((percent >= 80) && (percent <= 84.99)){
             gpa = (percent*3.66) / 80;
-            cout<<"Your GPA is: "<< gpa<< endl;
+
+            cout<<"Your GPA is: "<<setprecision(2)<< gpa<< endl;
             cout<<"Your Grade is \"A-\" "<< endl;
-            cout<<"Your Percentage is: "<< percent<< endl;
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
 
         }
          else if((percent >= 75) && (percent <= 79.99)){
-
             gpa = (percent*3.33) / 75;
-            cout<<"Your GPA is: "<< gpa<< endl;
-            cout<<"Your Grade is \"B+\" "<< endl;
-            cout<<"Your Percentage is: "<< percent<< endl;
-        }
-         else if((percent >= 75) && (percent <= 79.99)){
 
-            gpa = (percent*3.33) / 75;
-            cout<<"Your GPA is: "<< gpa<< endl;
+            cout<<"Your GPA is: "<<setprecision(2)<< gpa<< endl;
             cout<<"Your Grade is \"B+\" "<< endl;
-            cout<<"Your Percentage is: "<< percent<< endl;
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
+
+        }else if((percent >= 71) && (percent <= 74.99)){
+            gpa = (percent*3.00) / 71;
+
+            cout<<"Your GPA is: "<<setprecision(2)<< gpa<< endl;
+            cout<<"Your Grade is \"B\" "<< endl;
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
+
+        }else if((percent >= 68) && (percent <= 70.99)){
+            gpa = (percent*2.66) / 68;
+
+            cout<<"Your GPA is: "<<setprecision(2)<< gpa<< endl;
+            cout<<"Your Grade is \"B-\" "<< endl;
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
+
+        }else if((percent >= 61) && (percent <= 67.99)){
+            gpa = (percent*2.00) / 61;
+
+            cout<<"Your GPA is: "<<setprecision(2)<< gpa<< endl;
+            cout<<"Your Grade is \"C\" "<< endl;
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
+
+        }else if((percent >= 50) && (percent <= 60.99)){
+            gpa = (percent*1.00) / 50;
+
+            cout<<"Your GPA is: "<<setprecision(2)<< gpa<< endl;
+            cout<<"Your Grade is \"D\" "<< endl;
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
+
+        }else if(percent < 50){
+            gpa = 0.00;
+
+            cout<<"Your GPA is: "<<setprecision(2)<< gpa<< endl;
+            cout<<"Your Grade is \"F\" "<< endl;
+            cout<<"Your Percentage is: "<<setprecision(4)<<"%"<< percent<<endl;
         }
     }
     return 0;
